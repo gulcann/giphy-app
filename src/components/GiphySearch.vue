973536064dacaee1220d98d87ecc2fdc62856d7d
gulcann <gulcan.arik@gmail.com>
2019-11-25 00:31:21 +0300

    Added Load More button event and infinite scroll event.

--- src/components/GiphySearch.vue
+++ src/components/GiphySearch.vue
@@ -1,8 +1,46 @@
 <template>
   <div class="pl-3 pr-3 pa-3 pb-3">
-    <b-form-input class="mb-3" placeholder="Type to Search" type="search" id="filterInput"></b-form-input>
-    <div id="scrollspy-nested" style="position:relative; height:350px; overflow-y:auto">
-      <b-table striped hover bordered :items="items" id="giphy-table"></b-table>
+    <b-form-input
+      class="mb-3"
+      placeholder="Type to Search"
+      type="search"
+      id="filterInput"
+      v-model="searchTerm"
+    ></b-form-input>
+    <div
+      id="scrollspy-nested"
+      style="position:relative; height:350px; overflow-y:auto"
+      @scroll="onScroll"
+    >
+      <b-table
+        striped
+        hover
+        bordered
+        small
+        responsive="sm"
+        :items="items"
+        id="giphy-table"
+      >
+        <template v-slot:cell(images)="row">
+          <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+            {{ row.detailsShowing ? "Hide" : "Show" }}
+            Images Details
+          </b-button>
+        </template>
+
+        <template v-slot:row-details="row">
+          <b-card>
+            <b-row class="mb-2">
+              <b-col sm="3" class="text-sm-center"
+                ><b>{{ row.item.images }}</b></b-col
+              >
+            </b-row>
+            <b-button size="sm" @click="row.toggleDetails"
+              >Hide Images Details</b-button
+            >
+          </b-card>
+        </template>
+      </b-table>
     </div>
     <b-pagination
       align="right"
@@ -11,46 +49,58 @@
       :per-page="perPage"
       aria-controls="giphy-table"
     ></b-pagination>
-    <b-button class="pull-right" variant="outline-secondary">Load More</b-button>
+    <b-button
+      class="pull-right"
+      variant="outline-secondary"
+      @click="getSearchGiphies"
+      >Load More</b-button
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import GiphyService from "../service/giphy.service";
 
+@Component
 export default class GipgySearchComponent extends Vue {
-  private items: any[] = [
-    {
-      isActive: true,
-      age: 40,
-      first_name: "Dickerson",
-      last_name: "Macdonald"
-    },
-    { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-    { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
-    { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
-    {
-      isActive: true,
-      age: 40,
-      first_name: "Dickerson",
-      last_name: "Macdonald"
-    },
-    { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-    { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
-    { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" },
-    {
-      isActive: true,
-      age: 40,
-      first_name: "Dickerson",
-      last_name: "Macdonald"
-    },
-    { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-    { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
-    { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
-  ];
+  private items: any[] = [];
   private rows: number = this.items.length;
-  private perPage: number = 4;
+  private perPage: number = 20;
   private currentPage: number = 1;
+  private searchTerm: string = "";
+
+  async beforeMount() {
+    await this.getGiphies();
+  }
+  async getGiphies() {
+    await GiphyService.getTrendingGifs().then(data => {
+      this.items = data.data;
+    });
+  }
+  async getSearchGiphies() {
+    await GiphyService.getSearchGiphies(this.searchTerm).then(data => {
+      this.items = data.data;
+    });
+  }
+  async onScroll({
+    target: { scrollTop = 0, clientHeight = 0, scrollHeight = 0 }
+  }) {
+    if (scrollTop + clientHeight == scrollHeight) {
+      if (this.searchTerm) {
+        await GiphyService.getSearchGiphies(this.searchTerm).then(data => {
+          this.items = [...this.items, ...data.data];
+        });
+      } else {
+        await GiphyService.getTrendingGifs().then(data => {
+          this.items = [...this.items, ...data.data];
+          console.log("count:", this.items.length);
+        });
+      }
+      this.currentPage += 1;
+      console.log("page count:", this.currentPage);
+    }
+  }
 }
 </script>
 <style lang="postcss" scoped>
--- src/service/giphy.service.ts
+++ src/service/giphy.service.ts
@@ -1,9 +1,25 @@
 import axios from "axios";
-const API_URL = "https://api.giphy.com/v1/gifs/";
+const API_URL = "https://api.giphy.com/v1/gifs";
 
 export default class GiphyService {
   static async getTrendingGifs() {
     const url = `${API_URL}/trending?api_key=xrhlNmMdpunN0rQuGWkMnBkksLiecViV&limit=20&rating=G`;
-    return await axios.get(url).then(response => response.data);
+    let result = await axios
+      .get(url)
+      .then(response => response.data)
+      .catch(error => {
+        console.log(error);
+      });
+    return result;
+  }
+  static async getSearchGiphies(searchTerm: string) {
+    let url = `${API_URL}/search?api_key=xrhlNmMdpunN0rQuGWkMnBkksLiecViV&q=${searchTerm}&limit=25&offset=0&rating=G&lang=en`;
+    let result = await axios
+      .get(url)
+      .then(response => response.data)
+      .catch(error => {
+        console.log(error);
+      });
+    return result;
   }
 }
